9f1874dc59cc7ebeadea06c7d203a748736e3c05
Andy Chau <andychau@berkeley.edu>
2015-10-11 01:16:41 -0700

    Made boulder trigger

---
+++ Assets/BoulderTrig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderTrig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnTriggerEnter(Collider other) {
+//		if (other.gameObject.CompareTag("OVRPlayerController")){
+//			transform.Find("Boulder").Translate(100, -100, 100);
+//		}
+//	}
+		transform.Find("Boulder").Translate(100, -100, 100);
+		}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody player;
+	public float speed = 100f;
+
+	void Start () {
+		player = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+		player.AddForce(movement*speed);
+	}
+
+}
